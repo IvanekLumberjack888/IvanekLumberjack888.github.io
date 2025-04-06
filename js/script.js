dab531a2c43c9e09e412895cf5a6ea9616d2f956
Ivanek Lumberjack <ivousd@gmail.com>
2025-04-06 09:52:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const swiper = new Swiper('.swiper', {
-    // Nastavení carouselu
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+    },
 });
