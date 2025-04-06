325ae8e28fb6841b0e4c4b75f9aa8994a80eb003
Ivanek Lumberjack <ivousd@gmail.com>
2025-04-06 11:01:37 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 5000,
-    },
+document.addEventListener('DOMContentLoaded', function() {
+    const skills = document.querySelector('.skills');
+    const experience = document.querySelector('.experience');
+    const skillsList = skills.querySelector('ul');
+    const experienceList = experience.querySelector('ul');
+
+    skills.addEventListener('click', function() {
+        if (skillsList.style.display === 'none') {
+            skillsList.style.display = 'block';
+        } else {
+            skillsList.style.display = 'none';
+        }
+    });
+
+    experience.addEventListener('click', function() {
+        if (experienceList.style.display === 'none') {
+            experienceList.style.display = 'block';
+        } else {
+            experienceList.style.display = 'none';
+        }
+    });
 });
